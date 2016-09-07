0770d02bbb4036ae5d66319722085b90bbb236cb
Adam Zimmer <adamz@slalom.com>
2016-09-06 22:42:32 -0400

    step 4 half complete - right before deep linking

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,30 @@ import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
+
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import {routing } from './app.routing';
+
 
 
 @NgModule({
   imports:[
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [
     AppComponent
